c812222068b58e0856699a80ae7e75b3441db9f6
veggieboy <meanomenestcaesar@gmail.com>
2014-11-25 08:19:51 -0700

    Update Groceries.java

--- src/Groceries.java
+++ src/Groceries.java
@@ -1,1 +1,10 @@
-// main program to run the GroceryList
+import java.util*;
+public class Groceries
+{
+  public static void main(String [] args)
+  {
+    GroceryList list=new GroceryList();
+    GroceryItem i1=new GroceryItem("apple", 5, 6.60)
+    list.add(i1);
+  }
+}
