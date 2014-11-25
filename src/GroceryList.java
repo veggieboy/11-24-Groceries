acf4ad2def253af4589cb90a915422b63cd84550
veggieboy <meanomenestcaesar@gmail.com>
2014-11-25 08:14:00 -0700

    Update GroceryList.java

--- src/GroceryList.java
+++ src/GroceryList.java
@@ -1,1 +1,34 @@
-// class to store list of Grocery items 
+import java.util.*;
+public class GroceryList
+{
+  ArrayList<GroceryItem> list;
+  public GroceryList()
+  {
+  list=new ArrayList<GroceryItem>();
+  }
+  
+  public void add(GroceryItem item)
+  {
+    list.add(item);
+  }
+  
+  public double getTotalCost()
+  {
+    double netCost=0;
+    for (int i=0; i<list.size(); i++)
+    {
+    netCost+=list.get(i).getCost();
+    }
+    return netCost;
+  }
+  
+  public String toString()
+  {
+    String s="";
+    for (int i=0; i<list.size(); i++)
+    {
+      s+=list.get(i).toString()+"/n";
+    }
+    return s;
+  }
+} 
