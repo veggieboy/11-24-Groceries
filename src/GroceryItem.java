28463cc12b6389ff9e82cddbbf2b8b8eb942c8e8
veggieboy <meanomenestcaesar@gmail.com>
2014-11-25 08:07:07 -0700

    Update GroceryItem.java

--- src/GroceryItem.java
+++ src/GroceryItem.java
@@ -1,1 +1,28 @@
-// class to represent a grocery item
+public class GroceryItem
+{
+  String name;
+  int quantity;
+  double pricePerUnit;
+  
+  public GroceryItem (String name, int quantity, double pricePerUnit)
+  {
+    this.name=name;
+    this.quantity=quantity;
+    this.pricePerUnit=pricePerUnit;
+  }
+  
+  public double getCost()
+  {
+    return quantity*pricePerUnit;
+  }
+  
+  public  void setQuantity(int quantity)
+  {
+    this.quantity=quantity;
+  }
+  
+  public String toString()
+  {
+    return quantity+" @ $"+pricePerUnit+" = $"+quantity*pricePerUnit;
+  }
+}
