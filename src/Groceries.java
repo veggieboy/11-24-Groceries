6190139dc1fe3d4bd33839c512dc41bb105c95d8
veggieboy <meanomenestcaesar@gmail.com>
2014-11-25 18:19:27 -0700

    Update Groceries.java

--- src/Groceries.java
+++ src/Groceries.java
@@ -1,10 +1,18 @@
-import java.util*;
-public class Groceries
-{
-  public static void main(String [] args)
-  {
-    GroceryList list=new GroceryList();
-    GroceryItem i1=new GroceryItem("apple", 5, 6.60)
-    list.add(i1);
-  }
-}
+import java.util.*; 
+ public class Groceries 
+ { 
+   public static void main(String [] args) 
+   { 
+     GroceryList list=new GroceryList(); 
+     GroceryItem i1=new GroceryItem("apple", 5, 6.60); 
+     list.add(i1); 
+     GroceryItem i1=new GroceryItem("cheese", 6, 5.60); 
+     list.add(i2); 
+     GroceryItem i1=new GroceryItem("milk", 7, 8.20);
+     list.add(i3); 
+     GroceryItem i1=new GroceryItem("butter", 2, 1.40); 
+     list.add(i4); 
+     GroceryItem i1=new GroceryItem("bread", 3, 3.10); 
+     list.add(i5); 
+   } 
+ } 
